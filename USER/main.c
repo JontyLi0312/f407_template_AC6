f3af065a7410562d267f205354d5791acf3036c0
Jonty <ljt20030312@Outlook.com>
2023-08-29 11:51:32 +0800

    version 1.0 description: stm32f407的基于keil AC6 的模板工程

---
+++ USER/main.c
@@ -0,0 +1,17 @@
+/*** 
+ * @Author: Jonty ljt20030312@Outlook.com
+ * @Date: 2023-08-28 18:47
+ * @LastEditTime: 2023-08-28 20:27
+ * @FilePath: \template\USER\main.c
+ * @Description: 
+ */
+#include "stm32f4xx.h"
+#include "usart.h"
+#include "delay.h"
+
+int main(void)
+{
+
+}
+
+
